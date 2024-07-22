1b7220d4ef459f0091a2a5c733b68ca945ed229b
Upamanyu Mukharji <upamanyu.mukharji@hotmail.com>
2024-07-23 00:10:04 +0530

    add files

---
+++ src/test/java/com/controltower/mqtt_consumer/MqttConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.controltower.mqtt_consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MqttConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
